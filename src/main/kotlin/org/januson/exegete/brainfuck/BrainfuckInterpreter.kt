48edad6dcd556dfd05e31d2d9c175d2900acc418
Januson <janusonx@gmail.com>
2016-10-13 23:22:38 +0200

    Created basic class of Brainfuck interpreter.

---
+++ src/main/kotlin/org/januson/exegete/brainfuck/BrainfuckInterpreter.kt
@@ -0,0 +1,23 @@
+package org.januson.exegete.brainfuck
+
+/**
+ * Created by Januson on 13.10.2016.
+ */
+class BrainfuckInterpreter(val memorySize: Int) {
+
+    private val memory = ByteArray(memorySize)
+    private var pointer = 0
+
+    fun interpret(code: String) {
+        code.forEach { interpret(it) }
+    }
+
+    private fun interpret(instruction: Char) {
+        println(instruction)
+    }
+}
+
+fun main(args: Array<String>) {
+    val interpreter = BrainfuckInterpreter(2048)
+    interpreter.interpret("+++++>>>")
+}
